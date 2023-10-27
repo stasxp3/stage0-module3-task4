318e5107a10478eb50c00ec46c7279af2e104a63
Станислав <stasxp3@gmail.com>
2023-10-27 23:17:35 +0200

    stupid task

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+       /*
+       c 99	0000000001100011	63	143	&#99;
+       h 104	0000000001101000	68	150	&#104;
+       a 97	0000000001100001	61	141	&#97;
+       r 114	0000000001110010	72	162	&#114;
+        */
+    int c = 'c';
+    int h = 'h';
+    int a = 'a';
+    int r = 'r';
+    System.out.println('c');
+
     }
 }
